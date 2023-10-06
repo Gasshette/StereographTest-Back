3e14719ec7d43e2bcdc26ed2e7b307896472b915
Gasshette <gesshokun@gmail.com>
2023-10-06 14:43:40 +0200

    init(project): add base project
    Add the provided base project

---
+++ Stereograph.TechnicalTest.Api/Controllers/ProjectController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Stereograph.TechnicalTest.Api.Controllers;
+
+[Route("api/projects")]
+[ApiController]
+public class ProjectController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<String> Get()
+    {
+        return Ok("all");
+    }
+}
